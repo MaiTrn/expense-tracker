633d17c02e72f56c107a87b2798a9faad1ea853b
Mai Tran <maitran.ln99@gmail.com>
2022-12-05 14:19:42 +0200

    Set up styling for the app

---
+++ src/constants/theme.js
@@ -0,0 +1,80 @@
+export const COLORS = {
+  // base colors
+  primary: '#194868', // Dark Blue
+  secondary: '#FF615F', // peach
+
+  // colors
+  black: '#1E1F20',
+  white: '#FFFFFF',
+  lightGray: '#F5F7F9',
+  lightGray2: '#FAFAFA',
+  gray: '#BEC1D2',
+  blue: '#42B0FF',
+  darkGray: '#898C95',
+  darkGray2: '#FCFCFC',
+  yellow: '#FFD573',
+  lightBlue: '#95A9B8',
+  darkGreen: '#008159',
+  lightGreen: '#14B656',
+  peach: '#FF615F',
+  purple: '#8e44ad',
+  red: '#FF0000',
+  danger: '#FF0058',
+  valid: '#2CB9B0',
+};
+
+export const SIZES = {
+  // global sizes
+  base: 8,
+  font: 14,
+  padding: 20,
+  padding2: 30,
+  //border Radii
+  borderRadius: {
+    s: 4,
+    m: 10,
+    l: 20,
+    xl: 75,
+  },
+  //spacing
+  spacing: {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+
+  // font sizes
+  largeTitle: 72,
+  h1: 32,
+  h2: 24,
+  h3: 16,
+  h4: 14,
+  body1: 30,
+  body2: 20,
+  body3: 16,
+  body4: 14,
+};
+
+export const FONTS = {
+  h1: { fontSize: SIZES.h1, lineHeight: 36 },
+  h2: { fontSize: SIZES.h2, lineHeight: 30 },
+  h3: { fontSize: SIZES.h3, lineHeight: 22 },
+  h4: { fontSize: SIZES.h4, lineHeight: 22 },
+  body1: {
+    fontSize: SIZES.body1,
+    lineHeight: 36,
+  },
+  body2: {
+    fontSize: SIZES.body2,
+    lineHeight: 30,
+  },
+  body3: {
+    fontSize: SIZES.body3,
+    lineHeight: 22,
+  },
+  body4: {
+    fontSize: SIZES.body4,
+    lineHeight: 22,
+  },
+};
