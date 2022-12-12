772374430b17df5ee72d7c685f3b10de9b24a26d
Mai Tran <maitran.ln99@gmail.com>
2022-12-12 16:45:39 +0200

    Add calendar

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,26 +7,48 @@ import { fetchCategories } from '../redux/actions';
 import ExpenseList from './components/ExpenseList';
 import ExpenseChart from './components/ExpenseChart';
 import ExpenseSummary from './components/ExpenseSummary';
+import AddExpenseModal from '../expense-adder/AddExpenseModal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import '../constants/Calendar.css';
 
 const Home = (props) => {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     props.fetchCategories();
   }, []);
 
   return (
-    <div>
+    <div style={{ position: 'relative' }}>
       <Header />
       <div style={styles.container}>
         <div style={styles.expenseSummary}>
-          <ExpenseChart {...{ selectedCategory, setSelectedCategory }} />
+          <div
+            style={{
+              display: 'flex',
+              width: '56rem',
+              justifyContent: 'space-between',
+            }}
+          >
+            <ExpenseChart {...{ selectedCategory, setSelectedCategory }} />
+            <div style={{ height: 100, paddingTop: '5vh' }}>
+              <Calendar />
+            </div>
+          </div>
           <ExpenseSummary {...{ selectedCategory, setSelectedCategory }} />
+          <button style={styles.addButton} onClick={() => setOpen(true)}>
+            <FontAwesomeIcon icon={faPlus} color={COLORS.white} size='2xl' />
+          </button>
         </div>
         <div style={styles.expenseList}>
           <ExpenseList />
         </div>
       </div>
+      {open && <AddExpenseModal setOpen={setOpen} />}
     </div>
   );
 };
@@ -50,6 +72,21 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'relative',
   },
   expenseList: { flex: 3 },
+  addButton: {
+    width: 70,
+    height: 70,
+    borderRadius: '50%',
+    backgroundColor: COLORS.secondary,
+    position: 'absolute',
+    bottom: 0,
+    top: 0,
+    margin: 'auto 0',
+    right: 70,
+    border: 0,
+    boxShadow: `5px 5px 10px ${COLORS.darkGray}`,
+    cursor: 'pointer',
+  },
 };
